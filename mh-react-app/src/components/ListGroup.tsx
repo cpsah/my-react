28b02e0aae8dd4740084b526e15be7936ebe226d
chinmaysah <chinmaysah@gmail.com>
2024-03-20 22:09:00 +0530

    added bootstrap and list group

---
+++ mh-react-app/src/components/ListGroup.tsx
@@ -0,0 +1,19 @@
+function ListGroup() {
+  const items = [
+    'New York',
+    'San Francisco',
+    'Tokyo',
+    'London',
+    'Paris',
+  ];
+
+  return (
+    <>
+      <ul className="list-group">
+       {items.map(item => <li className="list-group-item" key={item}>{item}</li>)}
+      </ul>
+    </>
+  );
+}
+
+export default ListGroup;
